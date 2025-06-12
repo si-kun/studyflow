0b69e338ab338cd0210b9d4012512f492b571a7c
si-kun <si1kun.tails@gmail.com>
2025-06-13 07:18:14 +0900

    更新内容の説明

---
+++ src/app/(private)/page.tsx
@@ -0,0 +1,32 @@
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
+import { CircleSmall, Menu, Pencil, Search, Trash2 } from "lucide-react";
+
+const DAMMY_DATA = [
+  {
+    title: "学習メモ",
+    amount: 42,
+  },
+  {
+    title: "カテゴリー",
+    amount: 7,
+  },
+  {
+    title: "学習時間",
+    amount: 126,
+  },
+  {
+    title: "今月の記録",
+    amount: 15,
+  },
+];
+
+export default function Home() {
+  return (
+    <div></div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,9 +23,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ja">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased w-screen h-screen overflow-x-hidden p-5 bg-blue-400`}
       >
         {children}
       </body>
